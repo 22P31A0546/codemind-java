2bc3d05215c517cb3f001dcb3d52465fb161c51c
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2024-02-02 09:55:02 +0530

    import java.util.Scanner; public class hypo{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         double sum=Math.sqrt(a*a+b*b);         System.out.printf("%.2f",sum);     } }

--- Hypotenuse.java
+++ Hypotenuse.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
-public class Square
-{
+public class hypo{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int m=sc.nextInt();
-        int n=sc.nextInt();  
-        sc.close();
-        double hypotenuse=Math.sqrt(m*m+n*n);
-        System.out.printf("%.2f",hypotenuse);
-}
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double sum=Math.sqrt(a*a+b*b);
+        System.out.printf("%.2f",sum);
+    }
 }
