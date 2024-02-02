7243c49780b7770b615574a6d40b18a8c08a9ee4
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2024-02-02 11:01:34 +0530

    import java.util.*; public class Circle {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double c=sc.nextInt();         double sum=32+(c*9.0/5);         System.out.printf("%.2f",sum);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Circle
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double c=sc.nextInt();
+        double sum=32+(c*9.0/5);
+        System.out.printf("%.2f",sum);
+    }
+}
