65e11c869bb8b6859db763285a6f5efee5b97f73
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2024-02-13 10:35:10 +0530

    import java.util.Scanner; public class Solution {     public static void main(String[] args)     {         Scanner sc= new Scanner(System.in);         int x = sc.nextInt();         int y = sc.nextInt();         int z = sc.nextInt();         int a=((5*x)+(10*y))/z;         System.out.println(a);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        int a=((5*x)+(10*y))/z;
+        System.out.println(a);
+    }
+}
