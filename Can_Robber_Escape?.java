6deb4e4b6115fadce522c1b15149267f558033a1
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2024-02-15 09:52:12 +0530

    import java.util.*; public class Robber {     public static void main(String args[]){         int a,count=0;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         int b[] =new int[a];         for(int i=0;i<a;i++){             b[i]=sc.nextInt();             if(b[i]%2!=0)             count++;         }         if(count>2)         System.out.print("NO");         else         System.out.print("YES");     } }

---
+++ Can_Robber_Escape?.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Robber
+{
+    public static void main(String args[]){
+        int a,count=0;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        int b[] =new int[a];
+        for(int i=0;i<a;i++){
+            b[i]=sc.nextInt();
+            if(b[i]%2!=0)
+            count++;
+        }
+        if(count>2)
+        System.out.print("NO");
+        else
+        System.out.print("YES");
+    }
+}
