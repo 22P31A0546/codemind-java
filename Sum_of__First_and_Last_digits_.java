7e600bfe701624f71c2fe5f1f8b0af7bbfc04a0a
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2024-02-26 11:26:27 +0530

    import java.util.Scanner; public class SumOfFirstAndLast {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int b=n;         int f=0;         while(b!=0)         {             f=b;             b=b/10;         }         int l=n%10;         int s=f+l;         System.out.println(s);     } }

---
+++ Sum_of__First_and_Last_digits_.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumOfFirstAndLast
+{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int b=n;
+        int f=0;
+        while(b!=0)
+        {
+            f=b;
+            b=b/10;
+        }
+        int l=n%10;
+        int s=f+l;
+        System.out.println(s);
+    }
+}
