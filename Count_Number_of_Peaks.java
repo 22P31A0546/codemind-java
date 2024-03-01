a310f9dcd3d62c2075f308f8458e60ae4dab5451
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2024-03-01 11:08:42 +0530

    import java.util.*; public class Peaks{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         double s=0;         if(n==3)         {             System.out.println(10);         }         else if(n>3)         {           s=Math.pow(3,(n-3))*8*(n-2);           s+=s/4;             System.out.println((int)s);         }         else         {             System.out.println("0");         }     } }

---
+++ Count_Number_of_Peaks.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Peaks{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double s=0;
+        if(n==3)
+        {
+            System.out.println(10);
+        }
+        else if(n>3)
+        {
+          s=Math.pow(3,(n-3))*8*(n-2);
+          s+=s/4;
+            System.out.println((int)s);
+        }
+        else
+        {
+            System.out.println("0");
+        }
+    }
+}
